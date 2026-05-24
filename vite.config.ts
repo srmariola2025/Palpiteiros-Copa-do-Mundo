30a0a751edf2a9d761c58101f652e243e6da1f9f
Sr Mariola <srmariola@gmail.com>
2026-05-24 16:33:15 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Palpiteiros-Copa-do-Mundo/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
